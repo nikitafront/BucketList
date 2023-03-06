96e1ac8f1ec3bf67e232162392ce4e6aefc5e014
nikitafront <nikitapopov1295@gmail.com>
2023-03-06 20:13:09 +0500

    Initial commit

---
+++ app/src/main/java/com/webiki/bucketlist/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.webiki.bucketlist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.webiki.bucketlist.activities.MainActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+//        Thread.sleep(2000) //TODO: delete if main will long-loaded
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+
+    }
+}
