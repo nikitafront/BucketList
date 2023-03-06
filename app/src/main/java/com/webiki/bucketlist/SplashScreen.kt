4511b0dded4c7e067a37da1e9cdac3f7c83db7e3
nikitafront <nikitapopov1295@gmail.com>
2023-03-06 21:14:46 +0500

    add example of swapping screen (introductory form), random phrases on splash screen

--- app/src/main/java/com/webiki/bucketlist/SplashScreen.kt
+++ app/src/main/java/com/webiki/bucketlist/SplashScreen.kt
@@ -3,13 +3,22 @@ package com.webiki.bucketlist
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.webiki.bucketlist.activities.MainActivity
+import java.sql.Time
+import java.util.Calendar
+import kotlin.random.Random
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
+        val motivationText: TextView  =findViewById<TextView>(R.id.motivationText)
+        val texts = listOf("Motivation", "phrases", "into")
+        val rnd = Random(System.currentTimeMillis())
+        motivationText.setText( texts.get(rnd.nextInt( 3)) ) // not see because activity
+                                                                  // started before loading rnd
 //        Thread.sleep(2000) //TODO: delete if main will long-loaded
         startActivity(Intent(this, MainActivity::class.java))
         finish()
--- app/src/main/java/com/webiki/bucketlist/activities/MainActivity.kt
+++ app/src/main/java/com/webiki/bucketlist/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.webiki.bucketlist.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -42,12 +43,8 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-    }
+        startActivity(Intent(this, IntroductoryForm::class.java))
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
